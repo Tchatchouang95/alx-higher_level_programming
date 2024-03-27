28121a602558a2a7a3ce66c18cfe67e798623cb1
Tchatchouang95 <cedrickdjopgang@gmail.com>
2024-03-26 18:06:12 -0700

    Codefix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        return None
-    else:
-        size = len(sentence)
-        first = sentence[0]
-        return size, first
+    return len(sentence), sentence[0] if sentence else None
